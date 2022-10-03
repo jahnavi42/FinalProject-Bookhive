4041309607f0dda3c8ad872c187c7ca03fc949aa
dharshinik04 <dharshinik04@gmail.com>
2022-10-03 15:52:21 +0530

    admin

--- src/components/admin-addbook.component.js
+++ src/components/admin-addbook.component.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {BsPlusCircle} from 'react-icons/bs'
+import {BsPlusCircle,BsFileEarmarkImage} from 'react-icons/bs'
+import {MdHideImage} from 'react-icons/md'
 import { saveBook, showMsg } from '../reduxStore/main.slice'
 
 function AddBook(props) {
@@ -9,27 +10,31 @@ function AddBook(props) {
 
   let modify=props.modify
   let loadBook=useSelector(state=>state.main.viewingBook)
+  let initBookState={
+    "title": "",
+    "author":"",
+    "published":"",
+    "category":"",
+    "price":0,
+    "stock": 0,
+    "rating": 0,
+    "desc": "",
+    "image":""
+  }
   let [tempBook,setTempBook]=useState(
-    loadBook&&modify?{...loadBook}:{
-      "title": "",
-      "author":"",
-      "published":"",
-      "category":"",
-      "price":0,
-      "stock": 0,
-      "rating": 0,
-      "desc": "",
-      "image":""
-    }
+    loadBook&&modify?{...loadBook}:{...initBookState}
   )
 
-  
-  let originalTitle=tempBook.title
+  // saving this bcoz it acts as a key for the old book record
+  // BUG : this changes reactively hence won't work
+  // let originalTitle=tempBook.title.slice()
+  let [originalTitle,setOriginalTitle]=useState(tempBook.title)
+  console.log("ordiginal title is "+originalTitle)
   
 
   const handleFieldChange=(e,fieldName,prevValue)=>{
     let modTemp={...tempBook}
-    
+    // modTemp[fieldName]=typeof prevValue!="number"?e.target.value:parseInt(e.target.value)
     modTemp[fieldName]=e.target.value
     setTempBook(modTemp)
   }
@@ -39,11 +44,17 @@ function AddBook(props) {
   }
 
   const handleSaveBook=()=>{
+
     console.log({
       modTitle:originalTitle!=''?originalTitle:tempBook.title,
       modBook:tempBook
     })
+    if(tempBook.title==''){
+      dispatch(showMsg({msg:"Title required",type:"error"}))
+      return 
+    }
     dispatch(saveBook({
+      // modTitle:originalTitle!=''?originalTitle:tempBook.title,
       modTitle:originalTitle!=''?originalTitle:tempBook.title,
       modBook:{
         ...tempBook,
@@ -54,6 +65,8 @@ function AddBook(props) {
       },
       modified:modify
     }))
+    if(!modify)
+      setTempBook({...initBookState})
   }
 
   return (
@@ -62,13 +75,21 @@ function AddBook(props) {
       <div className="d-flex justify-content-center h1">{modify?"Edit Book":"Add Book"}</div>
       <div className="row">
         <div className="col-md-3" style={{marginLeft:"8%"}}>
-          <div className="row pb-3"><img src={tempBook.image} height="500px" width="150px"/></div>
-          <div className="row"><BsPlusCircle size={60}/></div>
+          {tempBook.image?.length>0&&
+            <div className="row pb-3">
+              <img src={tempBook.image} height="500px" width="150px" alt="Invalid Url"/>
+            </div>}
+          {!tempBook.image?.length>0&&
+            <div style={{color:'lightgray'}}>
+              <h3>Add Image Url<br/>to Preview</h3>
+              <BsFileEarmarkImage style={{height:"500px",width:"300px",color:"lightgray"}}/>
+            </div>
+          }
         </div>
         <div className="col-md-6" style={{marginLeft:"10%"}}>
           {tempBook&&Object.entries(tempBook).map(([fieldName,fieldValue])=>(
             <div className="col pb-2">
-                <h3 align="left">{fieldName}</h3>
+                <h3 align="left">{fieldName=="desc"?"description":fieldName}</h3>
                 <input 
                   className="form-control" 
                   type="text" id={fieldName} 
